ca6d803ebaaa09ff6f6d5d59bfd87e4b326bc8f3
Naveen <nkumartr1@in.ibm.com>
2020-12-17 15:48:01 +0530

    Intial commit

---
+++ src/main/java/com/coding/businessrules/CodingTestSvcApplication.java
@@ -0,0 +1,13 @@
+package com.coding.businessrules;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingTestSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodingTestSvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coding/businessrules/CodingTestSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coding.businessrules;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingTestSvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
